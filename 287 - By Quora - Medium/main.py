a649b404af1959f013fecec13046831feb635023
Amir Khazaie <733amir@gmail.com>
2020-12-18 20:06:30 +0330

    Solved the problem 287 with a naive solution

---
+++ 287 - By Quora - Medium/main.py
@@ -0,0 +1,39 @@
+from pprint import pprint
+
+
+def SolutionA(visits):
+    m = {}
+    for p in visits:
+        if p[0] not in m:
+            m[p[0]] = set()
+        m[p[0]].add(p[1])
+    print('m:')
+    pprint(m)
+    
+    sites = list(m.keys())
+    similarity = []
+    for i in range(len(sites)):
+        for j in range(i+1, len(sites)):
+            a, b = sites[i], sites[j]
+            ma, mb = m[a], m[b]
+            similarity.append([len(ma&mb), -len(ma^mb), a, b])
+    print('unordered similarity:')
+    pprint(similarity)
+
+    return sorted(similarity, reverse=True)
+
+def main():
+    visits = [
+        ('a', 1), ('a', 3), ('a', 5),
+        ('b', 2), ('b', 6),
+        ('c', 1), ('c', 2), ('c', 3), ('c', 4), ('c', 5),
+        ('d', 4), ('d', 5), ('d', 6), ('d', 7),
+        ('e', 1), ('e', 3), ('e', 5), ('e', 6)]
+    
+    similarity = SolutionA(visits)
+    print('similarity:')
+    pprint(similarity)
+
+
+if __name__ == "__main__":
+    main()
